b2d81dae9563ed2de2b03c20d30eb2a275c03a86
Chris Alley <chris@chrisalley.info>
2024-01-06 18:33:10 +1300

    Add a Preact Astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://steady-scone-f67e05.netlify.app'
-})
+  site: 'https://steady-scone-f67e05.netlify.app',
+  integrations: [preact()]
+});
